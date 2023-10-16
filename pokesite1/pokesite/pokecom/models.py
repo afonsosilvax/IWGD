06008223fa04b59c2f8133f81f3e2d18d3bf9ade
tomasfranciscoribeiro <99890336+tomasfranciscoribeiro@users.noreply.github.com>
2023-10-16 15:19:55 +0100

    funcional

--- pokesite1/pokesite/pokecom/models.py
+++ pokesite1/pokesite/pokecom/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class carta(models.Model):
+    Pokemon = models.CharField(max_length=200)
+    Raridade = models.CharField(max_length=200)
+    Coleção = models.CharField(max_length=200)
--- pokesite1/pokesite/pokecom/urls.py
+++ pokesite1/pokesite/pokecom/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path, include
 from . import views
 
+app_name = "pokecom"
 urlpatterns = [
     path('', views.index, name='index'),
+    path('pesquisar', views.pesquisar, name='pesquisar'),
+    path('vender', views.vender, name='vender'),
+    path('trocar', views.trocar, name='trocar'),
+    path('dispcarta', views.dispcarta, name='dispcarta'),
+    path('pesuisar_troca', views.pesquisar_troca, name='pesquisar_troca'),
+    path('login', views.loginview, name='login'),
+    path('criaruser', views.criaruser, name='criaruser'),
+    path('logout', views.fazlogout, name = 'logout'),
     ]
